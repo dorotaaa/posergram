54531b40108cd130d8b2e4007e7749d11b337eaa
dorota <dorota.sawicki@gmail.com>
2019-03-29 14:05:34 -0400

    clean-up

--- frontend/components/users/user_profile.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class UserProfile extends React.Component(){
-
-}
-
-export default(UserProfile)
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,20 +1,13 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
-import { RECEIVE_USER, RECEIVE_USERS } from '../actions/user_actions';
-import {merge} from 'lodash';
+import { merge } from 'lodash';
 
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return state;
-        case RECEIVE_USER:
-            return merge({}, state, { [action.currentUser.id]: action.user });
-        case RECEIVE_USERS:
-            return merge({}, state, action.users);
         default: return state;
     }
 };
 
 export default usersReducer;
-
-        
