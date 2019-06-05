f2298605303c2afcdcaaabca5c425477bf684304
dorota <dorota.sawicki@gmail.com>
2019-06-05 14:31:41 -0400

    follow backend done

---
+++ app/controllers/api/follows_controller.rb
@@ -0,0 +1,21 @@
+class Api::FollowsController < ApplicationController
+    before_action :ensure_login
+    
+    def create
+        @follow = Follow.new(follow_params)
+        @follow.follower_id = current_user.id 
+        @follow.save
+        render :show
+    end
+
+    def destroy
+        @follow = Follow.where(user_id: params[:id]).where(follower_id: current_user.id)[0]
+        @follow.destroy
+        render :show
+    end
+
+    def follow_params
+        params.require(:follow).permit(:user_id)
+    end 
+
+end
